f07c053e4d3e15cbcc59b13d987d776a3f032ee8
Lucy Dust <lucrezia.d.ast@protonmail.com>
2025-09-07 22:06:08 +0200

    activated pyright, deactivated rust_analyzer lsp servers

--- init.lua
+++ init.lua
@@ -632,8 +632,8 @@ require('lazy').setup({
         gopls = {
           filetypes = { 'go', 'kage' },
         },
-        -- pyright = {},
-        rust_analyzer = {},
+        pyright = {},
+        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
